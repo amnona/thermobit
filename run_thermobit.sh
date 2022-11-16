17bc4ddb362e0ffdc6e7147e50f274718a3306ce
amnona <amnonim@gmail.com>
2022-11-16 17:30:56 +0200

    add timer

--- run_thermobit.sh
+++ run_thermobit.sh
@@ -2,5 +2,5 @@
 
 source activate thermobit
 echo "running thermobit"
-cd ~/git/thermobit/thermobit
+cd /home/pi/git/thermobit/thermobit
 DEBUG=0 authbind gunicorn 'server:gunicorn(debug_level=4)' -b 0.0.0.0:80 --workers 1 --name=thermobit-server --reload
